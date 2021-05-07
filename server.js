d0fba472114329695d354d9b0bba97472c8b0246
nasfernane <nasfernane@gmail.com>
2021-05-07 09:56:06 +0200

    initialisation du projet

---
+++ server.js
@@ -0,0 +1,5 @@
+// librairie pour modéliser les données, gérer les queries etc
+const mongoose = require('mongoose');
+// module pour charger les var environnementales
+const dotenv = require('dotenv');
+
